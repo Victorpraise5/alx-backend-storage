f8b18046f865af3bec1c495cf090ab7b7b3d8a64
Victorpraise5 <victorpraise949@gmail.com>
2024-10-30 01:30:11 -0700

    Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,4 +7,6 @@ def update_topics(mongo_collection, name, topics):
     """
     Returns: none
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+       name_to_update = {"name": name}
+    new_topics = {"$set": {"topics": topics}}
+    mongo_collection.update_many(name_to_update, new_topics)
