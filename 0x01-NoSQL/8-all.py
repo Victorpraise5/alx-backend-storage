adb50957212024220d90cf5002cc322e4563a450
Victorpraise5 <victorpraise949@gmail.com>
2024-10-29 20:57:03 -0700

    List all documents in python

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,4 +7,8 @@ def list_all(mongo_collection):
     Returns:
     -A lis of all documents in the collection, or an empty list if no document is found
     """
-    return list(mongo_collection.find())
+    doc_list = list(mongo_collection.find())
+
+    if len(doc_list) is None:
+        return []
+    return doc_list
